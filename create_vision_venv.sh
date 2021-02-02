8b3e88b61d2cb6f4806ac6107092b49f34484450
rdkm89 <rdkm@cc.au.dk>
2021-02-02 14:48:53 +0100

    updated syllabus

--- create_vision_venv.sh
+++ create_vision_venv.sh
@@ -6,9 +6,12 @@ python3 -m venv $VENVNAME
 source $VENVNAME/bin/activate
 pip install --upgrade pip
 
+# problems when installing from requirements.txt
 pip install ipython
 pip install jupyter
+pip install matplotlib
 pip install opencv-python
+
 python -m ipykernel install --user --name=$VENVNAME
 
 test -f requirements.txt && pip install requirements.txt
