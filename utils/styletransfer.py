35af6b4cbe3f0e2f6a340d4cbf66911c61bbd1f1
rdkm89 <rdkm@cc.au.dk>
2021-04-21 20:39:52 +0200

    session11 materials

---
+++ utils/styletransfer.py
@@ -0,0 +1,42 @@
+# @title Define image loading and visualization functions  { display-mode: "form" }
+import functools, os
+from matplotlib import gridspec
+import matplotlib.pylab as plt
+import tensorflow as tf
+import numpy as np
+
+def crop_center(image):
+  """Returns a cropped square image."""
+  shape = image.shape
+  new_shape = min(shape[1], shape[2])
+  offset_y = max(shape[1] - shape[2], 0) // 2
+  offset_x = max(shape[2] - shape[1], 0) // 2
+  image = tf.image.crop_to_bounding_box(
+      image, offset_y, offset_x, new_shape, new_shape)
+  return image
+
+@functools.lru_cache(maxsize=None)
+def st_load(image_path, image_size=(256, 256), preserve_aspect_ratio=True):
+  """Loads and preprocesses images."""
+  # Load and convert to float32 numpy array, add batch dimension, and normalize to range [0, 1].
+  img = plt.imread(image_path).astype(np.float32)[np.newaxis, ...]
+  if img.max() > 1.0:
+    img = img / 255.
+  if len(img.shape) == 3:
+    img = tf.stack([img, img, img], axis=-1)
+  img = crop_center(img)
+  img = tf.image.resize(img, image_size, preserve_aspect_ratio=True)
+  return img
+
+def show_n(images, titles=('',)):
+  n = len(images)
+  image_sizes = [image.shape[1] for image in images]
+  w = (image_sizes[0] * 6) // 320
+  plt.figure(figsize=(w  * n, w))
+  gs = gridspec.GridSpec(1, n, width_ratios=image_sizes)
+  for i in range(n):
+    plt.subplot(gs[i])
+    plt.imshow(images[i][0], aspect='equal')
+    plt.axis('off')
+    plt.title(titles[i] if len(titles) > i else '')
+  plt.show()
