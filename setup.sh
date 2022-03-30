2987c3cd6626f392110552b38d52267bcd244cc4
Ross Deans Kristensen-McLachlan <rdkm@cc.au.dk>
2022-03-30 10:30:34 +0200

    updating setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 pip install --upgrade pip
-pip install opencv-python scikit-learn
+pip install opencv-python scikit-learn tensorflow tensorboard pydot
+sudo apt-get -y install graphviz
