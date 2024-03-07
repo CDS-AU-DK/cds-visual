362445ca194d2c562b958ce8c9824fabcc7adcf5
Ross Deans Kristensen-McLachlan <rdkm@cc.au.dk>
2024-03-07 11:29:35 +0100

    session 5 material

--- src/hello.py
+++ src/hello.py
@@ -1,8 +1,15 @@
 def hello(name:"World"):
+    """
+    A simple function to print a greeting to the screen
+    """
     print("Hello, " + name)
 
 def main():
+    """
+    The main logic of our program
+    """
     hello()
 
 if __name__=="__main__":
+    # "if this code is called from the command line, do the following"
     main()
