8c810ddbaa72d08e38f23db1122bf7e91f775b1a
Ross Deans Kristensen-McLachlan <rdkm@cc.au.dk>
2024-04-21 21:48:33 +0200

    session 11 material

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-sudo apt-get install -y python3-opencv
+sudo apt-get install libleptonica-dev tesseract-ocr tesseract-ocr-dev libtesseract-dev python3-pil tesseract-ocr-eng tesseract-ocr-script-latn
 pip install -r requirements.txt
