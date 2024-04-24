3c7043c68aa97f95575168a8b47257611184e634
Ross Deans Kristensen-McLachlan <rdkm@cc.au.dk>
2024-04-24 14:21:39 +0200

    session 12 material

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 
 # install required Linux binaries for OpenCV and Tesseract
 sudo apt-get install -y python3-opencv
-sudo apt install -y tesseract-ocr
-sudo apt install -y libtesseract-dev
+#sudo apt install -y tesseract-ocr
+#sudo apt install -y libtesseract-dev
 
 pip install -r requirements.txt
